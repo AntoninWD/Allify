1e070b0a27f3c1021a8e98a192ca889c0e76a07e
antoninWD <anto94@live.ca>
2021-03-26 17:08:18 -0400

    pomodoro part 3/4 done

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -1,14 +1,94 @@
 export const pomo = function () {
   // document selector
   const title = document.querySelector(".pomo__title");
-  const time = document.querySelector(".time");
+  const timeContainer = document.querySelector(".time");
   const workPlus = document.querySelector(".work__plus");
+  const workTime = document.querySelector(".work__time");
   const workMinus = document.querySelector(".work__minus");
   const breakPlus = document.querySelector(".break__plus");
+  const breakTime = document.querySelector(".break__time");
   const breakMinus = document.querySelector(".break__minus");
-  const play = document.querySelector(".play");
-  const reset = document.querySelector(".reset");
-  const pause = document.querySelector(".pause");
+  const playBtn = document.querySelector(".play");
+  const resetBtn = document.querySelector(".reset");
+  const pauseBtn = document.querySelector(".pause");
 
   ///
+  let wTime = 25;
+  let bTime = 5;
+  let firstStart = false;
+  let paused = false;
+  let set;
+  let resetPressed = false;
+  let configBlocked = false;
+  ////
+  const startTimer = function () {
+    configBlocked = true;
+    paused = false;
+    // wTime = wTime * 60;
+
+    if (!firstStart || resetPressed) {
+      set = setInterval(function () {
+        !paused ? workTimer() : "";
+      }, 1000);
+    }
+  };
+
+  const pauseTimer = function () {
+    paused = true;
+  };
+
+  const getTime = function () {
+    if (!firstStart) {
+      wTime = wTime * 60;
+      firstStart = true;
+    }
+    const min = String(Math.trunc(wTime / 60)).padStart(2, 0);
+    const sec = String(wTime % 60).padStart(2, 0);
+    timeContainer.textContent = `${min}:${sec}`;
+  };
+  const workTimer = function () {
+    getTime();
+    wTime--;
+  };
+
+  // event listener
+  workPlus.addEventListener("click", function () {
+    if (configBlocked) return;
+    wTime++;
+    workTime.textContent = `${wTime} mins`;
+  });
+
+  workMinus.addEventListener("click", function () {
+    if (configBlocked) return;
+    if (wTime === 0) return;
+    wTime--;
+    workTime.textContent = `${wTime} mins`;
+  });
+
+  breakPlus.addEventListener("click", function () {
+    if (configBlocked) return;
+    bTime++;
+    breakTime.textContent = `${bTime} mins`;
+  });
+
+  breakMinus.addEventListener("click", function () {
+    if (configBlocked) return;
+    if (bTime === 0) return;
+    bTime--;
+    breakTime.textContent = `${bTime} mins`;
+  });
+
+  resetBtn.addEventListener("click", function () {
+    clearInterval(set);
+    resetPressed = true;
+    wTime = 25;
+    workTime.textContent = `${wTime} mins`;
+    bTime = 5;
+    breakTime.textContent = `${bTime} mins`;
+    firstStart = false;
+    timeContainer.textContent = `25:00`;
+    configBlocked = false;
+  });
+  playBtn.addEventListener("click", startTimer);
+  pauseBtn.addEventListener("click", pauseTimer);
 };
